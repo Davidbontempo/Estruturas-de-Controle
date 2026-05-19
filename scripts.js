742c859d6e31e5f4edf9e33c7dcf6aa9d57194ed
David Bontempo <davidbontempobh@gmail.com>
2026-05-19 16:00:53 -0300

    Começando o js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+/*
+1) Pegar o valor informado.
+2) Pegar categoria informada.
+3) Impedir números negativos.
+4) Atualizar o valor de acordo com a categoria.
+    4.1)Criar variáveis para controlar ou armazenar os valores de cada uma das categorias. 
+5) Atualizar interface.
+6) Limpar Campos
+*/
+
+function adicionarGasto(){
+
+    console.log("Ok");
+}
