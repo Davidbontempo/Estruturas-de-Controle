3523646dd69136493fba1aa87de524041acb96c1
David Bontempo <davidbontempobh@gmail.com>
2026-05-20 09:39:52 -0300

    Funçoes do JS prontas!

--- scripts.js
+++ scripts.js
@@ -1,3 +1,42 @@
+
+const matrizGastos = [
+    ["Alimentação", 0],
+    ["Transporte", 0],
+    ["Lazer", 0],
+    ["Outros", 0],
+    ["Total", 0],
+]
+
+//Funções utilitárias
+const obterElemento = (id) => document.getElementById(id);
+const valorNegativo = (valor) => valor < 0;
+const somaValor = (total, valor) => total + valor;
+const limparCampos = () => obterElemento("valor").value = "";
+
+// Obter valores do formulário
+const obterValorInformado = () => obterElemento("valor");
+const obterCategoriaInformada = () => obterElemento("categoria").value;
+
+// Obter categoria da matriz
+const obterCategoria = (matriz, nomeCategoria) => matriz.find((item) => item[0] === nomeCategoria);
+
+//Atualiza valores da matriz
+const atualizaValorCategoria = (categoria, valor) => categoria[1] = somaValor(categoria[1], valor);
+
+const atualizarInterface = () => {
+
+    matrizGastos.forEach(([nome, valor]) =>{
+        const elemento = obterElemento(nome)
+        elemento.textContent = `${nome}: R ${valor}`
+    })
+}
+
+
+//Verificação da função no console
+function adicionarGasto(){
+    console.log("Ok");
+}
+
 /*
 1) Pegar o valor informado.
 2) Pegar categoria informada.
@@ -7,8 +46,3 @@
 5) Atualizar interface.
 6) Limpar Campos
 */
-
-function adicionarGasto(){
-
-    console.log("Ok");
-}
